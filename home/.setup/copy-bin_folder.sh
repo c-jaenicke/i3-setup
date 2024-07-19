e10ad383f119538602048c26845bcc3f9cabb7be
c-jaenicke <72254270+c-jaenicke@users.noreply.github.com>
2024-07-19 20:46:49 +0200

    Update copy-bin_folder.sh

--- home/.setup/copy-bin_folder.sh
+++ home/.setup/copy-bin_folder.sh
@@ -2,5 +2,5 @@
 printf "##### Copying bin folder\n"
 mkdir -p "$HOME/.bin"
 #cd "$current_directory" || printf "##### Failed to cd into repo dir" && exit
-cp ../.bin ~
+cp -r ../.bin ~
 printf "##### Copied bin folder\n"
