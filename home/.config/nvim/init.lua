7065bdaf8519d06c7d0465637e1764835abe94a0
c-jaenicke <c-jaenicke@pm.me>
2026-04-30 08:22:48 +0200

    up

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -473,6 +473,46 @@ require("lazy").setup({
     lazy = true,
     cmd = "DiffviewOpen"
 
+}, {
+    "nvim-pack/nvim-spectre",
+    lazy = true,
+    cmd = "Spectre"
+}, {
+  "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
 }, 
     -- ADD PLUGINS HERE
     },
